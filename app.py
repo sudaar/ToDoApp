393bfb64f956cdb790f649a40ae8f28fb9b455a1
suda <suda.yr25@gmail.com>
2018-06-30 15:36:42 +0900

    feat: add templates

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 # server.py
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+	todos =[]
+	return render_template('index.html', todos=todos)
 
 
 if __name__ == '__main__':
